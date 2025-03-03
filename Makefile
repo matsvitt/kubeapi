bd55b9da132a0cf48e043342ef8537fd6a561cd7
Matthias Vitt <matthias.vitt@gmail.com>
2025-03-03 10:51:20 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+default: run
+
+run:
+	uvicorn app:app --reload
+
+post1:
+	curl -X POST "http://127.0.0.1:8000/create" -H "Content-Type: application/json" -d '{"id": 1, "data": "example data"}'
+
+post2:
+	curl -X POST "http://127.0.0.1:8000/createshared" -H "Content-Type: application/json" -d '{"id": 2, "data": "shared data"}'
+
+get1:
+	curl -X GET "http://127.0.0.1:8000/read/1"
+
+get2:
+	curl -X GET "http://127.0.0.1:8000/readshared/2"
+
+
+build:
+	docker build -t kubeapi .
+
---
+++ app.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import logging
+import os
+import json
+
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+db = {}  # Simulated in-memory database
+db_shared = {}  # Simulated in-memory shared database
+
+class Item(BaseModel):
+    id: int
+    data: str
+
+@app.post("/create")
+def create_item(item: Item):
+    db[item.id] = item.data
+    
+    if os.path.exists("/data"):
+        with open(f"/data/data{item.id}.json","w") as f:
+            json.dump(item.model_dump_json(), f)
+            logging.info(f"Data saved to /data/data{item.id}.json") 
+    else:
+         logging.warning("/data directory does not exist")   
+    return {"message": "Item created", "id": item.id}
+
+@app.post("/createshared")
+def create_shared_item(item: Item):
+    db_shared[item.id] = item.data
+    if os.path.exists("/datashared"):
+        with open(f"/datashared/data{item.id}.json","w") as f:
+            json.dump(item.model_dump_json(), f)
+            logging.info(f"Data saved to /datashared/data{item.id}.json") 
+    else:
+         logging.warning("/data directory does not exist")  
+    return {"message": "Shared item created", "id": item.id}
+
+@app.get("/read/{id}")
+def read_item(id: int):
+    
+    if os.path.exists("f/data/data{id}.json"):
+        with open(f"/data/data{id}.json","r") as f:
+            data = json.load(f)
+            logging.info(f"Data read from /data/data{id}.json")
+            return data
+    elif id in db:
+        logging.info(f"Data read from in-memory database")
+        return {"id": id, "data": db[id]}
+    return {"error": "Item not found"}
+
+@app.get("/readshared/{id}")
+def read_shared_item(id: int):
+    
+    if os.path.exists("f/datashared/data{id}.json"):
+        with open(f"/datashared/data{id}.json","r") as f:
+            data = json.load(f)
+            logging.info(f"Data read from /datashared/data{id}.json")
+            return data    
+    elif id in db_shared:
+        logging.info(f"Data read from in-memory shared database")
+        return {"id": id, "data": db_shared[id]}
+    return {"error": "Shared item not found"}
+
+@app.get("/hello")
+def hello():
+    return "<html><body><h1>Hi there from fast api!</h1></body></html>"   
